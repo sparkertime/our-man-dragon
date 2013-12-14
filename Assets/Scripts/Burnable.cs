26c1bf90b630c6065636636af3bdac8094a8c8b6
sparker <scott.p.parker@gmail.com>
2013-12-14 17:55:53 -0600

    Orks can now collide with fire thanks to the BurnBox. What a fun problem...

---
+++ Assets/Scripts/Burnable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burnable : MonoBehaviour {
+
+	void OnParticleCollision(GameObject collision) {
+		Debug.Log("Something hit me! Fire?");
+	}
+}
